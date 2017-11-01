bff5dbce8d5390c92bc8188fac5961e2832b550b
michdebow <problemline@gmail.com>
2017-11-01 20:57:25 +0100

    Hover styles and autorefresh
    CSS: hover styles for tab links and th
    JS: autorefresh + auto tab expand

--- 17-18/functions.js
+++ 17-18/functions.js
@@ -1,29 +1,25 @@
 function toggle(className) {
 	var divsToHide = document.getElementsByClassName(className);
-    for (var i = 0; i < divsToHide.length; i++) {
-        divsToHide[i].style.display = divsToHide[i].style.display == "none" ? "table-cell"
+	for (var i = 0; i < divsToHide.length; i++) {
+		divsToHide[i].style.display = divsToHide[i].style.display == "none" ? "table-cell"
 				: "none";
+	}
+}
+
+function hideThree() {
+    var table = document.getElementsByTagName('table');
+    if (table[0].rows[1].cells.length < 69) {
+        for (var i = 0; i < table[0].rows.length; i++) {
+            table[0].rows[i].cells[3].style.display = 'none';
+        }
     }
 }
 
 
-function compare() {
-    var table = document.getElementsByTagName('table');
-    
-    if (table[0].rows[1].cells.length < 48) {
-        for (var i = 0; i < table[0].rows.length; i++) {
-            table[0].rows[i].cells[3].style.display = 'none';
-}
-}
-}
 
-function hideModal() {
-    var modal = document.querySelector('.modal');
-    setTimeout(function() {
-        modal.style.display = 'none';
-    }, 5);
-}
+toggle("colorful2");
+hideThree();
 
-
-
-
+setTimeout(function(){
+    location.reload();
+},60000);

5b1ced95b1dac11029d91a427153329d37b44a62
michdebow <problemline@gmail.com>
2017-11-01 20:43:41 +0100

    Add files via upload
    - CSS: added tab and th hover styles
    - JS: refresh every 60 secs

--- 17-18/functions.js
+++ 17-18/functions.js
@@ -1,7 +1,29 @@
 function toggle(className) {
 	var divsToHide = document.getElementsByClassName(className);
-	for (var i = 0; i < divsToHide.length; i++) {
-		divsToHide[i].style.display = divsToHide[i].style.display == "none" ? "table-cell"
+    for (var i = 0; i < divsToHide.length; i++) {
+        divsToHide[i].style.display = divsToHide[i].style.display == "none" ? "table-cell"
 				: "none";
-	}
+    }
 }
+
+
+function compare() {
+    var table = document.getElementsByTagName('table');
+    
+    if (table[0].rows[1].cells.length < 48) {
+        for (var i = 0; i < table[0].rows.length; i++) {
+            table[0].rows[i].cells[3].style.display = 'none';
+}
+}
+}
+
+function hideModal() {
+    var modal = document.querySelector('.modal');
+    setTimeout(function() {
+        modal.style.display = 'none';
+    }, 5);
+}
+
+
+
+

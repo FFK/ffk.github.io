88e39d93651f2270b12cc7524a4f1a96ebe40d1f
Mincer <faker.faker.klama@gmail.com>
2017-11-03 21:39:49 +0100

    Mincer update

--- 17-18/functions.js
+++ 17-18/functions.js
@@ -15,11 +15,32 @@ function hideThree() {
     }
 }
 
+function nextPage(page) {
+	switch (page) {
+		case 'PRO_MEN': return 'HARD_MEN';
+		case 'HARD_MEN': return 'EASY_MEN';
+		case 'EASY_MEN': return 'HARD_WOMEN';
+		case 'HARD_WOMEN': return 'EASY_WOMEN'; 
+		case 'EASY_WOMEN': return 'VETERAN_MEN'; 
+		case 'VETERAN_MEN': return 'VETERAN_WOMEN';
+		case 'VETERAN_WOMEN': return 'JUNIOR_MEN';
+		case 'JUNIOR_MEN': return 'JUNIOR_WOMEN';
+		case 'JUNIOR_WOMEN': return 'PRO_MEN'; 
+	}
+}
+
+function go() {
+	window.location.href += '?go=aye';
+}
 
 
 toggle("colorful2");
 hideThree();
 
 setTimeout(function(){
-    location.reload();
-},60000);
+	if(window.location.search) {}
+		var prefix = window.location.href.substring(0, location.href.lastIndexOf('/'));
+		var sufix = window.location.href.substring(window.location.href.lastIndexOf('/') + 1, window.location.href.lastIndexOf('.'));
+		window.location.href = prefix + '/' + nextPage(sufix) + '.html?go=aye';
+	}
+},10000);
